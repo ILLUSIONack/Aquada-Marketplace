ca2a2f2af385d9f3983a9a14a99dbd904c81c9c3
Vitalynx <selim@eveoh.nl>
2019-08-11 14:38:57 +0200

    integrate changes from master

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const nunjucks = require('nunjucks'); 
+const nunjucks = require('nunjucks');
 
 nunjucks.configure(__dirname + '/views', {
     autoescape: true,
@@ -13,8 +13,12 @@ app.use(express.static(__dirname + '/assets'));
 
 app.get('/', (req, res) => res.render('pages/index.html'));
 
-app.get('/search', (req, res) => res.render('pages/search.html', {search_query: "glycinate"}));
+app.get('/search', (req, res) => res.render('pages/search.html', {
+    search_query: "glycinate"
+}));
 
-app.get('/productdescription', (req, res) => res.render('pages/productdescription.html', {search_query: "glycinate"}));
+app.get('/product', (req, res) => res.render('pages/products.html'));
+
+app.get('/productdescription', (req, res) => res.render('pages/productdescription.html'));
 
 app.listen(port, () => console.log(`Aquada app listening on port ${port}!`))
